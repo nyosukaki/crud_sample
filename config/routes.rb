32be51b59331d74322a30b11b9e7f734a5622a99
nyosukaki <pikaru1994@gmail.com>
2019-10-29 22:09:09 +0900

    edit.html.erbの作成とroutes.rbをresourcesの利用

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # 以下のルーティングを追加
-  get 'users' => 'users#index'
-  get 'users/new' => 'users#new'
-  post 'users' => 'users#create'
-  delete 'users/:id' => 'users#destroy'
+  # get 'users' => 'users#index'
+  # get 'users/new' => 'users#new'
+  # post 'users' => 'users#create'
+  # delete 'users/:id' => 'users#destroy'
+  # get 'users/:id/edit' => 'users#edit'
+
+  resources :users
 end
