0b02fa19302d78cd886911c855ee58b977192c1c
nyosukaki <pikaru1994@gmail.com>
2019-10-29 20:46:31 +0900

    一覧画面に削除ボタン追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'users' => 'users#index'
   get 'users/new' => 'users#new'
   post 'users' => 'users#create'
-  delete 'users' => 'users#destroy'
+  delete 'users/:id' => 'users#destroy'
 end
